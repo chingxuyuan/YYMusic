4d767036e1aa0aea100627a0c785133e018f59e3
CXY <CXY@cxydemacbook-pro.local>
2016-04-12 18:36:14 +0800

    Initial Commit

---
+++ YYMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYMusic
+//
+//  Created by CXY on 16/4/12.
+//  Copyright © 2016年 CXY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
